207e719a68b8bed87bd863ae303a910b71a59fd4
Damola Olusakin <olusakinadedamola@gmail.com>
2023-05-30 01:20:44 -0400

    set up appwrite

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
+import Board from "@/components/Board";
 import Header from "@/components/Header";
 
-
 export default function Home() {
-  return <main>
-    {/*Header */}
-    <Header />
-    {/*Board */}
-    <h1>AIgility</h1>
-  </main>;
+  return (
+    <main>
+      <Header />
+      <Board />
+    </main>
+  );
 }
