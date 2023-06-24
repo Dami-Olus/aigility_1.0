dad6cc745ce306d00a030f8f9aafbffe25c7a427
Damola Olusakin <olusakinadedamola@gmail.com>
2023-06-24 01:17:16 -0400

    implemented GPT4 API

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,41 @@
 "use client";
 
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { logo } from "../assets";
 import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 import Avatar from "react-avatar";
 import { useBoardStore } from "@/store/BoardStore";
+import fetchSuggestion from "@/lib/fetchSuggestion";
 
 function Header() {
   const [board, searchString, setSearchString] = useBoardStore((state) => [
+    state.board,
     state.searchString,
     state.setSearchString,
-  ])
+  ]);
+
+  const [loading, setLoading] = useState<boolean>(false);
+  const [suggestion, setSuggestion] = useState<string>("");
+
+  useEffect(() => {
+    if (board.columns.size === 0) return;
+    setLoading(true);
+
+    const fetchSuggestionFunc = async () => {
+      const suggestion = await fetchSuggestion(board);
+      setSuggestion(suggestion);
+      setLoading(false);
+    };
+
+    fetchSuggestionFunc()
+  }, [board]);
+
   return (
     <header>
       <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
         <div
-        className="
+          className="
         absolute
         top-0
         left-0
@@ -47,7 +66,7 @@ function Header() {
               type="text"
               placeholder="search"
               value={searchString}
-              onChange={(e)=> setSearchString(e.target.value)}
+              onChange={(e) => setSearchString(e.target.value)}
               className="flex-1 outline-none p-2"
             />
             <button type="submit" hidden>
@@ -60,8 +79,8 @@ function Header() {
       </div>
       <div className="flex items-center justify-center px-5 py-2 md:py-5">
         <p className="flex items-center p-5 text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#233565]">
-          <UserCircleIcon className="inline-block h-10 w-10 text-[#233565] mr-1" />
-          GPT is summarising your tasks for the day
+          <UserCircleIcon className={`inline-block h-10 w-10 text-[#233565] mr-1 ${loading && "animate-spin"}`} />
+          {suggestion && !loading ? suggestion : "GPT is summarising your tasks for the day..."}
         </p>
       </div>
     </header>
